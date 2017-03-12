822fec306991e65927020c9db2068135c9e17402
Valters Jansons <valter.jansons@gmail.com>
2017-03-13 00:35:12 +0200

    Add Bower for front-end libraries
    We should be able to work with the small backend that we have laid
    out here. Time to move on to the front-end part of this. Might
    return to caching those TMDb queries later down the line - still
    got that Redis dependency laying around there - but need to get
    around to that. First, rough front-end.

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.get('/', (req, res) => {
   let data = { error: null };
