a31e2d0b1abbb6e3829dbebc9067e49759e2b5cd
Valters Jansons <valter.jansons@gmail.com>
2017-03-12 22:33:58 +0200

    Try to reconfigure once a day
    TMDb documentation recommends checking for updates every few days.
    We can comfortably check once every 24 hours. We can then check for
    when the service was started and comfortably predict when the
    configuration will be rechecked. It is just one API call per process
    right now anyway - not a big load, really.

--- server.js
+++ server.js
@@ -54,41 +54,57 @@ app.get('/', (req, res) => {
 });
 
 let models = require(path.join(__dirname, 'models'));
-tmdb.configuration()
-  .then(config => {
-    models.config.setUpImage(config.images.secure_base_url,
-      config.images.poster_sizes
-        .map(size => size.match(/^w([0-9]+)$/))
-        .filter(size => size)
-        .sort((a, b) => a[1] - b[1])
-        .slice(-1)[0][0],
-      config.images.profile_sizes
-        .map(size => size.match(/^w([0-9]+)$/))
-        .filter(size => size)
-        .sort((a, b) => a[1] - b[1])
-        .slice(-1)[0][0]);
 
-    let movies = require(path.join(__dirname, 'routes', 'movies'))(db, tmdb, models);
-    app.route('/m/search/:title')
-      .get(movies.search.get);
-    app.route('/m/:id')
-      .get(movies._.get);
+let configure = callback => {
+  tmdb.configuration()
+    .then(config => {
+      models.config.setUpImage(config.images.secure_base_url,
+        config.images.poster_sizes
+          .map(size => size.match(/^w([0-9]+)$/))
+          .filter(size => size)
+          .sort((a, b) => a[1] - b[1])
+          .slice(-1)[0][0],
+        config.images.profile_sizes
+          .map(size => size.match(/^w([0-9]+)$/))
+          .filter(size => size)
+          .sort((a, b) => a[1] - b[1])
+          .slice(-1)[0][0]);
 
-    let people = require(path.join(__dirname, 'routes', 'people'))(db, tmdb, models);
-    app.route('/p/search/:name')
-      .get(people.search.get);
-    app.route('/p/:id')
-      .get(people._.get);
+      callback(null);
+    })
+    .catch(err => callback(err));
+};
 
-    if (standalone) {
-      app.listen(app.get('port'));
-    }
-
-    app.emit('configured', null);
-  })
-  .catch(err => {
+configure(err => {
+  if (err) {
     throw err;
-  });
+  }
+
+  let movies = require(path.join(__dirname, 'routes', 'movies'))(db, tmdb, models);
+  app.route('/m/search/:title')
+    .get(movies.search.get);
+  app.route('/m/:id')
+    .get(movies._.get);
+
+  let people = require(path.join(__dirname, 'routes', 'people'))(db, tmdb, models);
+  app.route('/p/search/:name')
+    .get(people.search.get);
+  app.route('/p/:id')
+    .get(people._.get);
+
+  if (standalone) {
+    app.listen(app.get('port'));
+  }
+
+  app.emit('configured', null);
+});
+
+setInterval(configure, 1000/*ms*/ * 60/*s*/ * 60/*m*/ * 24/*h*/, err => {
+  if (err) {
+    // We do have some old configuration we can work off of and that should be fine, but do dump the error.
+    console.error('configure(err): ' + err.message);
+  }
+});
 
 if (typeof before === 'function') {
   before(done => app.on('configured', done));
