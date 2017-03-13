fa2600088927cc57ffe4bf804cd2e51f757404df
Valters Jansons <valter.jansons@gmail.com>
2017-03-13 11:26:54 +0200

    Add a view for showing the actor
    Quick draft coming in here. Don't mind me.

--- static/movielight.js
+++ static/movielight.js
@@ -4,12 +4,69 @@ $(function () {
   'use strict';
 
   var movieId = null;
+  var personId = null;
 
   $.fn.api.settings.api = {
     'search': '/m/search/{query}'
   };
 
   $('.ui.nag').nag('show');
+  $('.ui.modal').modal({ blurring: true });
+
+  var showMovie = function (id) {
+    movieId = id;
+    if (!id) { return; }
+
+    $.ajax('/m/' + id, {
+      dataType: 'json',
+      success: function (answer) {
+        if (!answer || !answer.movie || answer.movie.id !== movieId) { return; }
+        console.log(answer.movie);
+        $('.actors').empty();
+        var clickListener = function (e) {
+          showPerson($(e.currentTarget).data('id'));
+        };
+        for (var actor = 0; actor < answer.movie.cast.length; actor++) {
+          var person = answer.movie.cast[actor];
+          var column = $('<div class="clickable two wide column"><img class="ui fluid circular image"><br><p><b class="name"></b><br><em class="role"></em></p></div>');
+          column.data('id', person.id);
+          column.find('.image').attr('src', person.imageUrl);
+          column.find('.name').text(person.name);
+          column.find('.role').text(person.role);
+          column.click(clickListener);
+          $('.actors').append(column);
+        }
+      }
+    });
+  };
+
+  var showPerson = function (id) {
+    personId = id;
+    if (!id) { return; }
+
+    $.ajax('/p/' + id, {
+      dataType: 'json',
+      success: function (answer) {
+        if (!answer || !answer.person || answer.person.id !== personId) { return; }
+        console.log(answer.person);
+        var modal = $('.ui.modal');
+        modal.find('.header .name').text(answer.person.name);
+        modal.find('.header .aka').text(answer.person.alsoKnownAs && answer.person.alsoKnownAs.length > 0 ?
+            'also known as ' + answer.person.alsoKnownAs.join(', ') : '');
+        modal.find('.profile').attr('src', answer.person.imageUrl);
+        modal.find('.movies').empty();
+        for (var cast = 0; cast < answer.person.movies.length; cast++) {
+          var movie = answer.person.movies[cast];
+          var column = $('<div class="four wide movie column"><img class="ui fluid rounded image"><br><p><b class="title"></b><br><em class="year"></em></p></div>');
+          column.find('.image').attr('src', movie.posterUrl);
+          column.find('.title').text(movie.title);
+          column.find('.year').text(movie.year);
+          modal.find('.movies').prepend(column);
+        }
+        modal.modal('show');
+      }
+    });
+  };
 
   $('.ui.search').search({
     apiSettings: {
@@ -33,28 +90,10 @@ $(function () {
       image: 'posterUrl'
     },
     onSelect: function (result, res) {
-      movieId = result.id;
-      var title = result.title || '';
-      var originalTitle = result.originalTitle || '';
-      var year = result.year || '';
-
       $('.poster').attr('src', result.posterUrl || '');
-      $('.title').text(originalTitle + (year ? ' (' + year + ')' : ''));
-      $('.subtitle').text(title !== originalTitle ? title : '');
-
-      $.ajax('/m/' + result.id, {
-        dataType: 'json',
-        success: function (answer) {
-          if (!answer || !answer.movie || answer.movie.id !== movieId) { return; }
-          console.log(answer.movie);
-          $('.actors').empty();
-          for (var actor = 0; actor < answer.movie.cast.length; actor++) {
-            var person = answer.movie.cast[actor];
-            $('.actors').append('<div class="two wide column"><img class="ui fluid circular image" src="' + person.imageUrl + '"><br><p><b>' + person.name + '</b><br><i>' + person.role + '</i></p></div>');
-          }
-        }
-      });
-
+      $('.title').text(result.originalTitle + (result.year ? ' (' + result.year + ')' : ''));
+      $('.subtitle').text(result.title !== result.originalTitle ? result.title : '');
+      showMovie(result.id);
       return true;
     },
     onResultsOpen: function () {
