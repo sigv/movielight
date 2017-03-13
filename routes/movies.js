21c4033b782c65f7d2376f87e1d7fcaa1b65cfc2
Valters Jansons <valter.jansons@gmail.com>
2017-03-13 22:08:46 +0200

    Do naive caching on queries against movies
    We can just cache the whole responses for a short period of time so
    that we can return later responses much much quicker. It is probably
    a common occurance that people return to the same movie searches and
    that there are some queries that will be popular regardless and can
    benefit from global caching.

--- routes/movies.js
+++ routes/movies.js
@@ -8,28 +8,51 @@ module.exports = (db, tmdb, models) => {
     search: {
       get: (req, res) => {
         let data = { error: null, movies: [] };
-        let reply = () => res.status(data.error ? data.error.code || 500 : 200).json(data);
+        let setMovie = (movie) => {
+          data.movies = movie.results.map(movie => new models.Movie(movie.id,
+            movie.title,
+            movie.original_title,
+            movie.release_date ? parseInt(movie.release_date.split('-')[0], 10) : null,
+            movie.overview,
+            movie.poster_path));
+          res.status(data.error ? data.error.code || 500 : 200).json(data);
+        };
 
         let title = typeof req.params.title === 'string' ? req.params.title.trim() : '';
         if (title === '') {
           data.error = { code: 400, message: 'A searchable string title must be provided.' };
-          reply();
+          res.status(data.error.code).json(data);
         } else {
-          tmdb.search.movie({ query: title })
-            .then(movie => {
-              data.movies = movie.results.map(movie => new models.Movie(movie.id,
-                movie.title,
-                movie.original_title,
-                movie.release_date ? parseInt(movie.release_date.split('-')[0], 10) : null,
-                movie.overview,
-                movie.poster_path));
-              reply();
-            })
-            .catch(err => {
-              console.error('TMDb: ' + err.message);
-              data.error = { code: 503, message: 'The upstream API did not respond as expected.' };
-              reply();
-            });
+          let cacheKey = '/search/movie?' + title;
+          db.get(cacheKey, (err, movie) => {
+            if (err) {
+              console.error('Cache: ' + err.message);
+            } else if (movie) {
+              try {
+                setMovie(JSON.parse(movie));
+                console.log('Cache hit!');
+                return;
+              } catch (err) {
+                console.error('Cache: ' + err.message);
+              }
+            }
+
+            tmdb.search.movie({ query: title })
+              .then(movie => {
+                db.setex(cacheKey, 60/* s */ * 30/* min */, JSON.stringify(movie), err => {
+                  if (err) {
+                    console.error('Cache: ' + err.message);
+                  }
+
+                  setMovie(movie);
+                });
+              })
+              .catch(err => {
+                console.error('TMDb: ' + err.message);
+                data.error = { code: 503, message: 'The upstream API did not respond as expected.' };
+                res.status(data.error.code).json(data);
+              });
+          });
         }
       }
     },
@@ -37,32 +60,55 @@ module.exports = (db, tmdb, models) => {
     _: {
       get: (req, res) => {
         let data = { error: null, movie: {} };
-        let reply = () => res.status(data.error ? data.error.code || 500 : 200).json(data);
+        let setMovie = (movie) => {
+          data.movie = new models.Movie(movie.id,
+            movie.title,
+            movie.original_title,
+            movie.release_date ? parseInt(movie.release_date.split('-')[0], 10) : null,
+            movie.overview,
+            movie.poster_path,
+            movie.credits.cast.sort((a, b) => a.order - b.order).slice(0, 8).map(cast => new models.Character(cast.id,
+              cast.name,
+              cast.profile_path,
+              cast.character)));
+          res.status(data.error ? data.error.code || 500 : 200).json(data);
+        };
 
         let id = typeof req.params.id === 'string' ? parseInt(req.params.id, 10) : NaN;
         if (isNaN(id)) {
           data.error = { code: 400, message: 'A gettable integer ID must be provided.' };
-          reply();
+          res.status(data.error.code).json(data);
         } else {
-          tmdb.movie.details({ movie_id: id, append_to_response: 'credits' })
-            .then(movie => {
-              data.movie = new models.Movie(movie.id,
-                movie.title,
-                movie.original_title,
-                movie.release_date ? parseInt(movie.release_date.split('-')[0], 10) : null,
-                movie.overview,
-                movie.poster_path,
-                movie.credits.cast.sort((a, b) => a.order - b.order).slice(0, 8).map(cast => new models.Character(cast.id,
-                  cast.name,
-                  cast.profile_path,
-                  cast.character)));
-              reply();
-            })
-            .catch(err => {
-              console.error('TMDb: ' + err.message);
-              data.error = { code: 503, message: 'The upstream API did not respond as expected.' };
-              reply();
-            });
+          let cacheKey = '/movie/' + id;
+          db.get(cacheKey, (err, movie) => {
+            if (err) {
+              console.error('Cache: ' + err.message);
+            } else if (movie) {
+              try {
+                setMovie(JSON.parse(movie));
+                console.log('Cache hit!');
+                return;
+              } catch (err) {
+                console.error('Cache: ' + err.message);
+              }
+            }
+
+            tmdb.movie.details({ movie_id: id, append_to_response: 'credits' })
+              .then(movie => {
+                db.setex(cacheKey, 60/* s */ * 30/* min */, JSON.stringify(movie), err => {
+                  if (err) {
+                    console.error('Cache: ' + err.message);
+                  }
+
+                  setMovie(movie);
+                });
+              })
+              .catch(err => {
+                console.error('TMDb: ' + err.message);
+                data.error = { code: 503, message: 'The upstream API did not respond as expected.' };
+                res.status(data.error.code).json(data);
+              });
+          });
         }
       }
     }
